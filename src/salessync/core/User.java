da9399eb3f0453551b2715f21e4f72bbe8974591
DJFCoder <juanfabianmarcal@gmail.com>
2024-10-11 18:35:38 -0300

    configureUser

---
+++ src/salessync/core/User.java
@@ -0,0 +1,59 @@
+package salessync.core;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+
+    private UUID userId;
+    private String login, password;
+    private LocalDateTime lastLogin;
+    private Position position;
+
+    public User(String login, String password) {
+        this.userId = UUID.randomUUID();
+        this.login = login;
+        this.password = password;
+    }
+    
+    
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
