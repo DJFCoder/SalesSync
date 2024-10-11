46c8e7855862858c68ed1a9681efbb2958b8c321
DJFCoder <juanfabianmarcal@gmail.com>
2024-10-11 00:41:43 -0300

    firstCommit

---
+++ src/salessync/SalesSync.java
@@ -0,0 +1,16 @@
+package salessync;
+
+import salessync.register.Profile;
+
+public class SalesSync {
+
+    public static void main(String[] args) {
+        /* Controle de Usuários: 
+        
+        - Gerenciamento de contas e permissões dos usuários internos pelo Administrador do Sistema. */
+        
+        Profile admin = new Profile();
+                        
+    }
+    
+}
